33a2b0152f720fda90b2f608a6f92864ee75e237
anshuu23 <anshum@serviots.com>
2025-03-20 19:11:32 +0530

    fix : solved debounsing err

--- pagination-searching/backend/app/repository/user.ts
+++ pagination-searching/backend/app/repository/user.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
-export default async function insertDetails(id : number , first_name : string , last_name : string , email : string , gender : string , ip_address : string){
-    try{
+export default async function insertDetails(id: number, first_name: string, last_name: string, email: string, gender: string, ip_address: string) {
+    try {
         return await prisma.user.create({
-            data:{
+            data: {
                 id,
-                first_name ,
+                first_name,
 
                 last_name,
                 gender,
@@ -14,53 +14,61 @@ export default async function insertDetails(id : number , first_name : string , 
                 ip_address
             }
         })
-    
+
     }
-    catch(er){
+    catch (er) {
         return 0
     }
-    
+
 }
 
-export async function getData(skip : number , orderByy : string  , sortIn : string , take : number ) {
-    const data =  await prisma.user.findMany({
-        skip ,
-        take ,
-        orderBy :{
-            [orderByy] : sortIn
+export async function getData(skip: number, orderByy: string, sortIn: string, take: number) {
+    const data = await prisma.user.findMany({
+        skip,
+        take,
+        orderBy: {
+            [orderByy]: sortIn
         }
     })
 
-    return {count : 1000 , data}
+    return { count: 1000, data }
 }
 
-export async function getDataBasedOnFirstName(skip : number , orderByy : string  , sortIn : string , first_name: string , take : number) {
+export async function getDataBasedOnFirstName(skip: number, orderByy: string, sortIn: string, first_name: string, take: number) {
 
-    const count =  await prisma.user.count({
+    let whereData;
+    if (first_name.length === 1) {
+        whereData = "startsWith"
+    } else {
+        whereData = "contains"
+    }
 
-        where:{
-            first_name:{
-                contains: first_name, 
+    const count = await prisma.user.count({
+
+        where: {
+            first_name: {
+                [whereData]: first_name,
                 mode: "insensitive",
+
             }
         }
     })
 
-    const data =  await prisma.user.findMany({
-        skip ,
-        take ,
-        
-        orderBy :{
-            [orderByy] : sortIn
+    const data = await prisma.user.findMany({
+        skip,
+        take,
+
+        orderBy: {
+            [orderByy]: sortIn
         },
-        where:{
-            first_name:{
-                contains: first_name, 
+        where: {
+            first_name: {
+                [whereData]: first_name,
                 mode: "insensitive",
             }
         }
 
     })
-    return {count , data}
+    return { count, data }
 
 }
--- pagination-searching/frontend/src/App.tsx
+++ pagination-searching/frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import React, { useState , useEffect } from 'react';
-import type { TableColumnsType, TableProps , Input } from 'antd';
-import { Button, Space, Table } from 'antd';
-import { getData } from './helper/functions';
-import { FilterValue } from 'antd/es/table/interface';
+import React, { useState, useEffect } from "react";
+import type { TableColumnsType, TableProps } from "antd";
+import { Table } from "antd";
 
-type OnChange = NonNullable<TableProps<DataType>['onChange']>;
-type Filters = Parameters<OnChange>[1];
+type OnChange = NonNullable<TableProps<DataType>["onChange"]>;
 
 type GetSingle<T> = T extends (infer U)[] ? U : never;
 type Sorts = GetSingle<Parameters<OnChange>[2]>;
@@ -19,114 +16,58 @@ interface DataType {
   ip_address: string;
 }
 
-
-
 const App: React.FC = () => {
-
-  const [filteredInfo, setFilteredInfo] = useState<Filters>({});
   const [sortedInfo, setSortedInfo] = useState<Sorts>({});
-  const [dataArr , setData] = useState([])
+  const [dataArr, setData] = useState([]);
   const data: DataType[] = dataArr;
 
-  const [dataCount , setDataCount] = useState(1000)
-  const [pageCount , setPageCount] = useState(10)
+  const [dataCount, setDataCount] = useState(1000);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [take, setTake] = useState(10);
+  const [orderBy, setOrderBy] = useState("id");
+  const [sortIn, setSortIn] = useState("asc");
+  const [first_name, setFirst_name] = useState("");
 
-  const [currentPage , setCurrentPage] = useState(1)
-  const [take , setTake] = useState(10)
-  const [orderBy , setOrderBy] = useState('id')
-  const [sortIn , setSortIn] = useState('asc')
-  const [first_name , setFirst_name] = useState('')
+  useEffect(() => {
+    // fetch(
+    //   `http://localhost:3000/getData?currentPage=${currentPage}&orderBy=${orderBy}&sortIn=${sortIn}&first_name=${first_name}&take=${take}`
+    // )
+    //   .then((data) => data.json())
+    //   .then((data) => {
+    //     console.log("data-----------", data);
+    //     setData(data.data);
+    //     setDataCount(data.count);
+    //   });
 
-  useEffect(()=>{
-    fetch(`http://localhost:3000/getData?currentPage=${currentPage}&orderBy=${orderBy}&sortIn=${sortIn}&first_name=${first_name}&take=${take}`)
-    .then(data => data.json())
-    .then((data)=>{
-      console.log('data-----------' , data)
-      setData(data.data)
-      setDataCount(data.count)
-    })
-  },[currentPage , take , sortIn , orderBy  ,first_name])
+    
+   
+  }, [currentPage, take, sortIn, orderBy, first_name,]);
 
-  // let typingTimeout : any
+  let typingTimeout;
 
-  // const onChangeOfName = (e) =>{
-  //   e.target.name = e.target.value
-  //   setNameToSend(e.target.value)
-  //   console.log(e.target.value)
+  function onNameChange(e) {
+    console.log(first_name);
+    clearTimeout(typingTimeout);
 
-  //   clearTimeout(typingTimeout)
-  //       typingTimeout = setTimeout(()=>{
-  //           console.log("you are typing")
-
-  //           fetch(`http://localhost:3000/getData?first_name=${e.target.value}&takee=${pageCount}`)
-  //           .then(data => data.json())
-  //           .then((data)=>{
-  //           console.log(data)
-  //           setData(data.data)
-  //           setDataCount(data.count)
-  //           console.log("datacount---------" , data.count)
-  //           })
-
-  //       } , 500)
-  //   }
-  
-  function onNameChange(e){
-    setFirst_name(e.target.value)
-    console.log(first_name)
+    typingTimeout = setTimeout(() => {
+      console.log("you are typing");
+      setFirst_name(e.target.value);
+    }, 1000);
   }
 
-  const handleChange: OnChange = async (pagination ,filters, sorter ) => {
-    console.log('Various parameters', pagination);
-    console.log('Various filters', filters);
-    console.log('Various filters', sorter);
-    
-    setCurrentPage(pagination.current ?? 1)
-    setTake(pagination.pageSize ?? 10)
-    setOrderBy( sorter.columnKey ?? 'id')
-    setSortIn(sorter?.order  ?? null)
-    console.log(currentPage , take , orderBy , sortIn )
+  const handleChange: OnChange = async (pagination, filters, sorter) => {
+    console.log("Various parameters", pagination);
+    console.log("Various filters", filters);
+    console.log("Various filters", sorter);
 
-    //const res = await getData(currentPage , take , orderBy , sortIn , first_name )
-
-    // if(sortIn == "ascend"){
-    //   sortIn = "asc"
-    // }
-    // else{
-    //   sortIn = "desc"
-    // }
-    // console.log('333333333',pagination.pageSize)
-    // let skip = 0
-    // let idk =  pagination.pageSize ? pagination.pageSize : 10
-    // if(page != 1){
-    //    skip = (page - 1) * idk
-    // }
-    // else{
-    //    skip = 0
-    // }
-    // console.log(page , orderBy , sortIn)
-    // setPageCount(pagination.pageSize)
-    // if(!orderBy){
-    //   console.log("skipp------" , skip)
-    //   fetch(`http://localhost:3000/getData?skip=${skip}&takee=${pagination.pageSize}`)
-    //   .then(data => data.json())
-    //   .then((data)=>{
-    //   console.log(data)
-    //   setData(data.data)
-    //   })
-    // }else{
-    //   fetch(`http://localhost:3000/getData?skip=${skip}&orderBy=${orderBy}&sortIn=${sortIn}&first_name=${nameTOSend}&takee=${pagination.pageSize}`)
-    //   .then(data => data.json())
-    //   .then((data)=>{
-    //   console.log(data)
-    //   setData(data.data)
-    //   })
-    // }
-    
+    setCurrentPage(pagination.current ?? 1);
+    setTake(pagination.pageSize ?? 10);
+    setOrderBy(sorter.columnKey ?? "id");
+    setSortIn(sorter?.order ?? "asc");
+    console.log(currentPage, take, orderBy, sortIn);
 
     setSortedInfo(sorter as Sorts);
-
   };
-
   const clearFilters = () => {
     setFilteredInfo({});
   };
@@ -138,78 +79,83 @@ const App: React.FC = () => {
 
   const setAgeSort = () => {
     setSortedInfo({
-      order: 'descend',
-      columnKey: 'age',
+      order: "descend",
+      columnKey: "age",
     });
   };
 
   const columns: TableColumnsType<DataType> = [
     {
-      title: 'id',
-      dataIndex: 'id',
-      key: 'id',
+      title: "roll no",
+      dataIndex: "id",
+      key: "id",
       sorter: (a, b) => a.id - b.id,
-      sortOrder: sortedInfo?.columnKey === 'id' ? sortedInfo.order : null,
+      sortOrder: sortedInfo?.columnKey === "id" ? sortedInfo.order : null,
       ellipsis: true,
     },
     {
-      title: 'First Name',
-      dataIndex: 'first_name',
-      key: 'first_name',
+      title: "First Name",
+      dataIndex: "first_name",
+      key: "first_name",
       sorter: () => 0,
-      sortOrder: sortedInfo?.columnKey === 'first_name' ? sortedInfo.order : null,
+      sortOrder:
+        sortedInfo?.columnKey === "first_name" ? sortedInfo.order : null,
       ellipsis: true,
     },
-    
+
     {
-      title: 'Last Name',
-      dataIndex: 'last_name',
-      key: 'last_name',
-      sorter: () => 0 ,
-      sortOrder: sortedInfo?.columnKey === 'last_name' ? sortedInfo.order : null,
+      title: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
+      sorter: () => 0,
+      sortOrder:
+        sortedInfo?.columnKey === "last_name" ? sortedInfo.order : null,
       ellipsis: true,
     },
-    
+
     {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-      sorter: () => 0 ,
-      sortOrder: sortedInfo?.columnKey === 'email' ? sortedInfo.order : null,
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      sorter: () => 0,
+      sortOrder: sortedInfo?.columnKey === "email" ? sortedInfo.order : null,
       ellipsis: true,
     },
-    
+
     {
-      title: 'Gender',
-      dataIndex: 'gender',
-      key: 'gender',
-      sorter: () => 0 ,
-      sortOrder: sortedInfo?.columnKey === 'gender' ? sortedInfo.order : null,
-      ellipsis: true,
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
     },
-    
+
     {
-      title: 'IP address',
-      dataIndex: 'ip_address',
-      key: 'ip_address',
-      sorter: () => 0 ,
-      sortOrder: sortedInfo?.columnKey === 'ip_address' ? sortedInfo.order : null,
+      title: "IP address",
+      dataIndex: "ip_address",
+      key: "ip_address",
+      sorter: () => 0,
+      sortOrder:
+        sortedInfo?.columnKey === "ip_address" ? sortedInfo.order : null,
       ellipsis: true,
     },
-    
   ];
 
   return (
     <>
-    <h1>Pagination</h1>
-      <input type="text" placeholder='enter first name' name='first_Name' onChange={onNameChange}/>
+      <h1>Pagination</h1>
+      <input
+        type="text"
+        placeholder="enter first name"
+        name="first_Name"
+        onChange={onNameChange}
+      />
 
-      
-      <Table<DataType> columns={columns} dataSource={data} onChange={handleChange} pagination={{total:dataCount,
-      defaultCurrent:1,      
-      }} />
+      <Table<DataType>
+        columns={columns}
+        dataSource={data}
+        onChange={handleChange}
+        pagination={{ total: dataCount, defaultCurrent: 1 }}
+      />
     </>
   );
-
-}
+};
 export default App;
